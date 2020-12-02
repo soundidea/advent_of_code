b4a8e2e82b225ebd8a8bf8bd6ed6ef0e5c5ba9f2
Alistair Milne <sound.idea@gmail.com>
2020-12-01 21:49:17 -0800

    Readability tweak

--- 2_solution.py
+++ 2_solution.py
@@ -5,5 +5,6 @@ from collections import Counter
 if __name__ == '__main__':
   with open('2_input.txt') as f:
     passwords = [re.match(r'(\d+)-(\d+) ([a-z]): (\w+)', line).groups() for line in f.readlines()]
-  print 'part 1: %d' % len(filter(lambda p: int(p[0]) <= Counter(p[3])[p[2]] <= int(p[1]), passwords))
-  print 'part 2: %d' % len(filter(lambda p: Counter((p[3][int(p[0])-1], p[3][int(p[1])-1]))[p[2]] == 1, passwords))
+  passwords = [(int(p[0]), int(p[1]), p[2], p[3]) for p in passwords]
+  print 'part 1: %d' % len(filter(lambda p: p[0] <= Counter(p[3])[p[2]] <= p[1], passwords))
+  print 'part 2: %d' % len(filter(lambda p: Counter((p[3][p[0]-1], p[3][p[1]-1]))[p[2]] == 1, passwords))

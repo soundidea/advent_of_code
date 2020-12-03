d7ac875171f76af955847d21f3874ec195dc5843
Alistair Milne <sound.idea@gmail.com>
2020-12-02 22:09:59 -0800

    Advent of code, day 3

--- 2_solution.py
+++ 2_solution.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
   with open('2_input.txt') as f:
     passwords = [re.match(r'(\d+)-(\d+) ([a-z]): (\w+)', line).groups() for line in f.readlines()]
   passwords = [(int(p[0]), int(p[1]), p[2], p[3]) for p in passwords]
-  print 'part 1: %d' % len([True for p in passwords if p[0] <= len([True for c in p[3] if c == p[2]]) <= p[1]])
-  print 'part 2: %d' % len([True for p in passwords if (p[3][p[0]-1] == p[2]) ^ (p[3][p[1]-1] == p[2])])
+  print 'part 1: %d' % sum(1 for p in passwords if p[0] <= p[3].count(p[2]) <= p[1])
+  print 'part 2: %d' % sum(1 for p in passwords if (p[3][p[0]-1] == p[2]) ^ (p[3][p[1]-1] == p[2]))
---
+++ 3_solution.py
@@ -0,0 +1,20 @@
+from functools import partial
+from itertools import count
+
+def gen_coords((dx,dy), max_x, max_y):
+  return zip(map(lambda x: x % max_x, range(0, max_y * dx / dy, dx)), range(0, max_y, dy))
+
+
+def trees_for_slope(grid, (x,y)):
+    return sum(1 for (x,y) in gen_coords((x,y), len(grid[0]), len(grid)) if grid[y][x] == '#')
+
+
+def product(seq):
+  return reduce(lambda a, b: a * b, seq)
+
+
+if __name__ == '__main__':
+  with open('3_input.txt') as f:
+    grid = [l.rstrip() for l in f.readlines()]
+  print 'part 1: %d' % trees_for_slope(grid, (3,1))
+  print 'part 2: %d' % product(map(partial(trees_for_slope, grid), [(1,1), (3,1), (5,1), (7,1), (1,2)]))

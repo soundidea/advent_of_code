ff1e7f9d19fd0d60eac0dfe92c08c1c00ff7b623
Alistair Milne <sound.idea@gmail.com>
2020-12-04 21:46:32 -0800

    Advent of code, day 5.

--- 5_solution.py
+++ 5_solution.py
@@ -1,3 +1,5 @@
+import re
+
 
 def sum_to(n):
   return n * (n+1) / 2
@@ -5,8 +7,9 @@ def sum_to(n):
 
 if __name__ == '__main__':
   with open('5_input.txt') as f:
-      seats = [8 * int(p[:7].replace('F', '0').replace('B', '1'), 2)
-                 + int(p[7:10].replace('R', '1').replace('L', '0'), 2)
-               for p in f.readlines()]
+    seats = [int(re.sub(r'(B|R)', '1', re.sub(r'(F|L)', '0', l[:10])), base=2) for l in f.readlines()]
+#      seats = [8 * int(p[:7].replace('F', '0').replace('B', '1'), 2)
+#                 + int(p[7:10].replace('R', '1').replace('L', '0'), 2)
+#               for p in f.readlines()]
   print 'part 1: %d' % max(seats)
   print 'part 2: %d' % (sum_to(max(seats)) - sum_to(min(seats)-1) - sum(seats))

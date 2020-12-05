77f53b528f8b87b79361b346580755a281ac4048
Alistair Milne <sound.idea@gmail.com>
2020-12-04 22:01:30 -0800

    Advent of Code, day 5.

--- 5_solution.py
+++ 5_solution.py
@@ -1,12 +1,8 @@
-import re
-
-
 def sum_to(n):
   return n * (n+1) / 2
 
-
 if __name__ == '__main__':
   with open('5_input.txt') as f:
-    seats = [int(re.sub(r'(B|R)', '1', re.sub(r'(F|L)', '0', l[:10])), base=2) for l in f.readlines()]
+    seats = [reduce(lambda a, b: a * 2 + (1 if b in ['B','R'] else 0), l[:10], 0) for l in f.readlines()]
   print 'part 1: %d' % max(seats)
   print 'part 2: %d' % (sum_to(max(seats)) - sum_to(min(seats)-1) - sum(seats))

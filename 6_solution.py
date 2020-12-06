a3fc95a6b8593c13df2ba2dfcca0f299f92234c3
Alistair Milne <sound.idea@gmail.com>
2020-12-05 21:39:41 -0800

    Advent of code, day 6.

--- 6_solution.py
+++ 6_solution.py
@@ -1,7 +1,6 @@
 
 if __name__ == '__main__':
   with open('6_input.txt') as f:
-    groups = [g.rstrip().split('\n') for g in f.read().split('\n\n')]
-
-  print 'part 1: %d' % sum(len(reduce(lambda a, b: set(a).union(set(b)), g)) for g in groups)
-  print 'part 2: %d' % sum(len(reduce(lambda a, b: set(a).intersection(set(b)), g)) for g in groups)
+    groups = [[set(p) for p in g.rstrip().split('\n')] for g in f.read().split('\n\n')]
+  print 'part 1: %d' % sum(len(reduce(lambda a, b: a.union(b), g)) for g in groups)
+  print 'part 2: %d' % sum(len(reduce(lambda a, b: a.intersection(b), g)) for g in groups)

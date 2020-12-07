c28d23e2129abb0396d574f61e5e413cc44b07b9
Alistair Milne <sound.idea@gmail.com>
2020-12-07 13:59:36 -0800

    Cleaned up the parsing code a bit by using re.findall instead of re.match

--- 7_solution.py
+++ 7_solution.py
@@ -7,7 +7,8 @@ import re
 # Output: ('faded blue', {})
 def parse_bag(bag):
   name, contents = re.match(r'^(.*) bags contain (.*)$', bag).groups()
-  return (name, dict((c[1], int(c[0])) for c in re.findall('(\d+) (.+?) bags?(?:,|\.)', contents)))
+  return (name, dict((c[1], int(c[0])) for c in re.findall('(\d+) (.+?) bag', contents)))
+  #return (name, dict((c[1], int(c[0])) for c in re.findall('(\d+) (.+?) bags?(?:,|\.)', contents)))
 
 
 def recursive_parents(bags, bag_name):

0b8399b0caf3463c73fab8892016ce5146afe4c9
Alistair Milne <sound.idea@gmail.com>
2020-12-13 15:45:11 -0800

    added a comment explaining what 'buses' looks like after parsing

--- 13_solution.py
+++ 13_solution.py
@@ -4,6 +4,7 @@ from operator import mul
 with open('13_input.txt') as f:
   timestamp, buses = [l.strip() for l in f]
   buses = sorted([(int(bus), idx) for idx, bus in enumerate(buses.split(',')) if bus != 'x'], reverse=True)
+  # with the test input, buses = [(7, 0), (13, 1), (59, 4), (31, 6), (19, 7)]
 
 print 'part 1: %d' % reduce(mul, min((bus - (int(timestamp) % bus), bus) for bus,_ in buses))
 print 'part 2: %d' % reduce(lambda (t, step), (bus, offs): next((c, step * bus) for c in count(t, step) if (c + offs) % bus == 0), buses, (0, 1))[0]

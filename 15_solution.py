2028ce98dc328a35106f10628464f360c19e7126
Alistair Milne <sound.idea@gmail.com>
2020-12-15 09:46:54 -0800

    Don't need a defaultdict after-all, since dict.get has a default arg.

--- 15_solution.py
+++ 15_solution.py
@@ -1,9 +1,7 @@
-from collections import defaultdict
 inputs = (16,11,15,0,1,7)
 for part, count_to in enumerate((2020, 30000000)):
-  last_seen = defaultdict(lambda: i, {n:i for i,n in enumerate(inputs[:-1])})
+  last_seen = {n:i for i,n in enumerate(inputs[:-1])}
   prev = inputs[-1]
   for i in range(len(inputs) - 1, count_to - 1):
-      last_seen[prev], prev = i, i - last_seen[prev]
+    last_seen[prev], prev = i, i - last_seen.get(prev, i)
   print 'part %d: %d' % (part + 1, prev)
-

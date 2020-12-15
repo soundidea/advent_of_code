206d34d9b7013d15c9f3ec0c1723d2c588e67d10
Alistair Milne <sound.idea@gmail.com>
2020-12-15 00:03:05 -0800

    Eliminated the if/else by using a defaultdict that returns i by default.

--- 15_solution.py
+++ 15_solution.py
@@ -1,11 +1,9 @@
+from collections import defaultdict
 inputs = (16,11,15,0,1,7)
 for part, count_to in enumerate((2020, 30000000)):
-  last_indices = {n:i for i,n in enumerate(inputs[:-1])}
+  last_seen = defaultdict(lambda: i, {n:i for i,n in enumerate(inputs[:-1])})
   prev = inputs[-1]
   for i in range(len(inputs) - 1, count_to - 1):
-    if prev in last_indices:
-      last_indices[prev], prev = i, i - last_indices[prev]
-    else:
-      last_indices[prev], prev = i, 0
+      last_seen[prev], prev = i, i - last_seen[prev]
   print 'part %d: %d' % (part + 1, prev)
 

c13305478b90112c644103358ec534c04aa7617a
Alistair Milne <sound.idea@gmail.com>
2020-12-17 14:20:44 -0800

    Goes faster to only consider the neighbours of the currently active cells, rather than the entire grid. Also eliminates the need for the limits function.

--- 17_solution.py
+++ 17_solution.py
@@ -1,3 +1,4 @@
+from functools import partial
 from itertools import product
 import numpy as np
 
@@ -11,12 +12,8 @@ def set_active(grid, coord):
   return (is_currently_active and 2 <= num_active_neighbours <= 3) \
       or (not is_currently_active and num_active_neighbours == 3)
 
-def limits(grid):
-  dims = len(next(iter(grid)))
-  return [(min(c[axis] for c in grid), 1 + max(c[axis] for c in grid)) for axis in range(dims)]
-
 with open('17_input.txt') as f:
   grid = set((x,y,0) for y,row in enumerate(f) for x,cell in enumerate(row) if cell == '#')
-one_turn = lambda grid,_: set(c for c in product(*map(lambda (l1,l2): range(l1-1,l2+1), limits(grid))) if set_active(grid, c))
+one_turn = lambda grid,_: {c for c in {n for c in grid for n in neighbours(c)} | grid if set_active(grid, c)}
 print 'part 1: %d' % len(reduce(one_turn, range(6), grid))
 print 'part 2: %d' % len(reduce(one_turn, range(6), set(g + (0,) for g in grid)))

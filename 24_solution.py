972c4ecbaf3a0e0e9e548f8366c9ba2026e26692
Alistair Milne <sound.idea@gmail.com>
2020-12-24 00:37:29 -0800

    Using a set instead of an array makes it go much much faster.

--- 24_solution.py
+++ 24_solution.py
@@ -1,36 +1,31 @@
 import re
 from collections import Counter
-from itertools import chain
 
 # Represent the hex grid as a skewed rectangular grid.
 # See: https://www.redblobgames.com/grids/hexagons/#coordinates-axial
-do_move = {
-  'e': lambda (x,y): (x+1,y),
-  'w': lambda (x,y): (x-1,y),
-  'ne': lambda (x,y): (x+1,y+1),
-  'nw': lambda (x,y): (x,y+1),
-  'se': lambda (x,y): (x,y-1),
-  'sw': lambda (x,y): (x-1,y-1),
+directions = {
+  'e': 1 + 0j,
+  'w': -1 + 0j,
+  'ne': 1 + 1j,
+  'nw': 0 + 1j,
+  'se': 0 - 1j,
+  'sw': -1 - 1j
 }
-
-def neighbours((x,y), include_center=False):
-  if include_center: return [(x,y), (x+1,y), (x-1,y), (x+1,y+1), (x,y+1), (x,y-1), (x-1,y-1)]
-  else:              return [(x+1,y), (x-1,y), (x+1,y+1), (x,y+1), (x,y-1), (x-1,y-1)]
+offsets = directions.values()
 
 def evolve_floor(black_tiles, num_days):
   for _ in range(num_days):
-    candidates = set(chain.from_iterable(neighbours(t,True) for t in black_tiles))
-    new_day = []
+    candidates = black_tiles.union(t + offset for t in black_tiles for offset in offsets)
+    next_day = set()
     for tile in candidates:
-      num_black_neighbours = sum(1 for n in neighbours(tile) if n in black_tiles)
-      if ((tile in black_tiles and 1 <= num_black_neighbours <= 2)
-          or (tile not in black_tiles and num_black_neighbours == 2)):
-        new_day.append(tile)
-    black_tiles = new_day
+      blk_n = sum(tile + offset in black_tiles for offset in offsets)
+      if (tile in black_tiles and 1 <= blk_n <= 2) or (tile not in black_tiles and blk_n == 2):
+        next_day.add(tile)
+    black_tiles = next_day
   return black_tiles
 
 flip_tiles = [re.findall(r'(se|sw|ne|nw|e|w)', l) for l in open('24_input.txt')]
-flip_tiles = [reduce(lambda c,d: do_move[d](c), moves, (0,0)) for moves in flip_tiles]
-black_tiles = [coord for (coord, num_flips) in Counter(flip_tiles).items() if (num_flips % 2)]
+flip_tiles = [reduce(lambda coord, move: coord + directions[move], moves, 0 + 0j) for moves in flip_tiles]
+black_tiles = {coord for (coord, num_flips) in Counter(flip_tiles).items() if (num_flips % 2)}
 print 'part 1:', len(black_tiles)
 print 'part 2:', len(evolve_floor(black_tiles, 100))

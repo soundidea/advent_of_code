3125bb275c99541597d99c7688ea96faf26d9b4b
Alistair Milne <sound.idea@gmail.com>
2021-12-20 18:00:39 -0800

    rename 'node' to 'room', remove commented-out line

--- y2021/12_solution.py
+++ y2021/12_solution.py
@@ -2,11 +2,10 @@ from collections import Counter
 import re
 
 connections = [re.match(r'(\w+)-(\w+)\n', line).groups() for line in open('12_input.txt').readlines()]
-#connections = [re.match(r'(\w+)-(\w+)\n', line).groups() for line in open('12_test_input_1.txt').readlines()]
-graph = {node: [r for c in connections
+graph = {room: [r for c in connections
                   for r in c
-                  if node in c and r not in [node, 'start']]
-         for node in set(r for c in connections for r in c)}
+                  if room in c and r not in [room, 'start']]
+         for room in set(r for c in connections for r in c)}
 graph['end'] = []
 
 def num_small_repeats(path):
@@ -17,12 +16,12 @@ def complete_paths(graph, max_small_repeats):
   while len(active_paths):
     new_paths = []
     for path in active_paths:
-      next_rooms = [node for node in graph[path[-1]]
-                         if node.isupper() or 
-                            node not in path or
+      next_rooms = [room for room in graph[path[-1]]
+                         if room.isupper() or
+                            room not in path or
                             num_small_repeats(path) < max_small_repeats]
       if len(next_rooms):
-        new_paths.extend(path + [node] for node in next_rooms)
+        new_paths.extend(path + [room] for room in next_rooms)
       elif path[-1] == 'end':
         completed_paths.append(path)
     active_paths = new_paths

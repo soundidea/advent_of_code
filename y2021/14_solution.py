cbcaa0e36694d217968cfb0ccbb706382a9ffec2
Alistair Milne <sound.idea@gmail.com>
2021-12-20 22:05:29 -0800

    Day 14

--- y2021/14_solution.py
+++ y2021/14_solution.py
@@ -5,9 +5,7 @@ template, rules = map(str.strip, open('14_input.txt').read().split('\n\n'))
 rules = {r[:2] : (''.join([r[0], r[6]]), ''.join([r[6], r[1]]))
          for r in rules.split('\n')}
 
-polymer = list(''.join(pair) for pair in zip(template[:-1], template[1:]))
-
-def apply_steps(polymer, num_steps):
+def apply_steps(template, num_steps):
   '''Given a template as a list of letter pairs (e.g. ['NN', 'NC', 'CB'] for NNCB)
      Apply rule substitution `num_steps` times, and return the letter frequencies
      of the resulting polymer.'''
@@ -16,13 +14,14 @@ def apply_steps(polymer, num_steps):
                           [Counter({child: count for child in rules[pair]})
                               for pair, count in pc.items()]),
       range(num_steps),
-      Counter(polymer))
+      Counter(template))
   return reduce(lambda c, pc: c + Counter({pc[0][1]: pc[1]}),
                 pair_counts.items(),
-                Counter(polymer[0]))
+                Counter(template[0][0]))
 
-counts = apply_steps(polymer, 10)
+template = list(''.join(pair) for pair in zip(template[:-1], template[1:]))
+counts = apply_steps(template, 10)
 print(max(counts.values()) - min(counts.values()))
-counts = apply_steps(polymer, 40)
+counts = apply_steps(template, 40)
 print(max(counts.values()) - min(counts.values()))
 

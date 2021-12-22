9bc41df9318ab92b58b41f2b7fe022d9a41620f1
Alistair Milne <sound.idea@gmail.com>
2021-12-22 15:08:58 -0800

    Figured out how to calculate the final positions of the probes directly instead of iteratively, which is a big speedup.

--- y2021/17_solution.py
+++ y2021/17_solution.py
@@ -2,12 +2,16 @@ from itertools import dropwhile, product
 from math import ceil, sqrt
 import re
 
-def next_pos(initial_velocity):
-  pos, vel = (0,0), initial_velocity
-  while True:
-    pos = (pos[0] + vel[0], pos[1] + vel[1])
-    vel = (max(0, vel[0] - 1), vel[1] - 1)
-    yield pos
+def final_pos(x_vel, y_vel, t_left, t_top):
+  '''Given an initial velocity (x_vel, y_vel) calculate where the probe
+     will be when it passes the top left of the target zone.'''
+  b = (2 * x_vel + 1) / 2
+  xt = int(ceil(b - sqrt(b**2 - 2 * t_left)))
+  b = (2 * y_vel + 1) / 2
+  yt = int(ceil(b + sqrt(b**2 - 2 * t_top)))
+  t = max(xt, yt)
+  xt = min(x_vel, t)
+  return ((2 * x_vel + 1 - xt) * xt // 2, (2 * y_vel + 1 - t) * t // 2)
 
 target = list(map(int, re.search(r'target area: x=([-\d]+)..([-\d]+), y=([-\d]+)..([-\d]+)',
                                  open('17_input.txt').read()).groups()))
@@ -20,8 +24,6 @@ x_vels = range(int(ceil(sqrt(2 * t_left + 0.25) - 0.5)), t_right + 1)
 target_zone = set(product(range(t_left, t_right + 1), range(t_bottom, t_top + 1)))
 
 print('part 1:', t_bottom * (t_bottom+1) // 2)
-print('part 2:', sum(next(dropwhile(lambda pos: pos[0] < t_left or pos[1] > t_top,
-                                    next_pos(u)))
-                     in target_zone
-                     for u in product(x_vels, y_vels)))
+print('part 2:', sum(final_pos(*init_velocity, t_left, t_top) in target_zone
+                     for init_velocity in product(x_vels, y_vels)))
 

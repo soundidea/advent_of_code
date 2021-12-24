c363e1ae0d0ad1e7f401073f2f4f720dace195ef
Alistair Milne <sound.idea@gmail.com>
2021-12-24 04:22:29 -0800

    Comments and tidyup

--- y2021/24_solution.py
+++ y2021/24_solution.py
@@ -3,19 +3,21 @@ def seek(params, find_largest=True, z=0, idx=0):
     return (z == 0, [])
   i,j,k = params[idx]
   if i == 26:
-    # we have to choose the digit that makes z smaller
-    # if it doesn't exist, we must bail or z will never reach 0
     magic = z % 26 + j
     if magic > 9 or magic < 1:
+      # Backtrack, z will never reach 0 from here.
       return False, []
     success, n = seek(params, find_largest, z // i, idx + 1)
-    return success, [z % 26 + j] + n
+    # No need to search for this digit, just use the one that makes z smaller.
+    return success, [magic] + n
   else:
+    # Need to search through the 9 possibilities for this digit.
     digits = range(9,0,-1) if find_largest else range(1,10)
     for digit in digits:
       success, n = seek(params, find_largest, z * 26 + digit + k, idx + 1)
       if success:
         return success, [digit] + n
+  # Dead end, backtrack.
   return False, []
 
 # Each block of 18 lines is almost identical, it just differs in the params on lines 4,5,15, so just gather those

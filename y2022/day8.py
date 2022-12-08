c83551e56c567fea8fb934e1afbcc2667f75f3a1
Alistair Milne <soundidea@users.noreply.github.com>
2022-12-08 14:24:34 -0800

    Faster solution, turns the grid into a dict of complex coord => tree height, then turns each tree into an array of four tuple(Bool,Int), meaning if you go in a certain direction from the tree: (Bool) do you reach the edge? and (int) how many trees do you see?

--- y2022/day8.py
+++ y2022/day8.py
@@ -1,24 +1,24 @@
-from itertools import takewhile
 from math import prod
+from functools import partial
 
-def rays(x,y,w,h,grid):
-  return (
-    [grid[y][rx] for rx in range(x-1, -1, -1)],  # left from x
-    [grid[y][rx] for rx in range(x+1, w)],       # right from x
-    [grid[ry][x] for ry in range(y-1, -1, -1)],  # up from y
-    [grid[ry][x] for ry in range(y+1, h)])       # down from y
+def visibility(grid, coord, step):
+  '''Walks the grid from coord in direction step.
+     Stops if a tall enough tree is encountered.
+     grid is a dict { Complex coord: Int tree height }
+     Returns (Bool, Int) => (reached the edge?, num trees seen)'''
+  n = 0
+  h = grid[coord]
+  coord += step
+  while coord in grid:
+    n += 1
+    if grid[coord] >= h:
+      return (False, n)
+    coord += step
+  return (True, n)
 
-def num_can_see(h, seq):
-  return min(len(seq), 1 + len(list(takewhile(lambda tree: tree < h, seq))))
-
-grid = [list(map(int, row.strip())) for row in open('day8_input.txt').readlines()]
-w,h = len(grid[0]), len(grid)
-print('part 1:', sum(any(max(ray) < grid[y][x] if len(ray) else True
-                         for ray in rays(x,y,w,h,grid))
-                     for x in range(w)
-                     for y in range(h)))
-print('part 2:', max(prod(num_can_see(grid[y][x], ray)
-                          for ray in rays(x,y,w,h,grid))
-                     for x in range(w)
-                     for y in range(h)))
-
+grid = {complex(x,y) : int(tree)
+        for y,row in enumerate(open('day8_input.txt').read().split('\n'))
+        for x,tree in enumerate(row)}
+trees = [[partial(visibility, grid, c)(step) for step in (-1, 1, -1j, 1j)] for c in grid]
+print('part 1:', sum(any(direction[0] for direction in tree) for tree in trees))
+print('part 2:', max(prod(direction[1] for direction in tree) for tree in trees))

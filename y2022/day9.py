0a5e6b49549a0199208d686f527bde62b1a3eb75
Alistair Milne <soundidea@users.noreply.github.com>
2022-12-08 22:19:03 -0800

    day 9, python

---
+++ y2022/day9.py
@@ -0,0 +1,20 @@
+def sign(x):
+  return (x > 0) - (x < 0)
+
+def simulate(moves, n):
+  knots = [0j for _ in range(n)]
+  visited = set([0j])
+  for m in moves:
+    for _ in range(int(m[2:])):
+      knots[0] += {'L': -1, 'R': 1, 'U': 1j, 'D': -1j}[m[0]]
+      for k in range(1,n):
+        d = knots[k-1] - knots[k]
+        if abs(d) > 1.42:
+          knots[k] += complex(sign(d.real), sign(d.imag))
+      visited.add(knots[-1])
+  return len(visited)
+
+moves = open('day9_input.txt').read().strip().split('\n')
+print('part 1:', simulate(moves, 2))
+print('part 2:', simulate(moves, 10))
+

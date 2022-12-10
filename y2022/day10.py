7d4614a940da618f3b47090236328f1cd123b0a4
Alistair Milne <soundidea@users.noreply.github.com>
2022-12-10 15:12:18 -0800

    day 10, python

--- y2022/day10.py
+++ y2022/day10.py
@@ -1,13 +1,14 @@
 from functools import reduce
-_, hist = reduce(lambda xh, i:
-                     (xh[0] + int(i[5:]), xh[1] + [xh[0], xh[0]])
-                     if i[0] == 'a'
-                     else (xh[0], xh[1] + [xh[0]]),
-                 open('day10_input.txt').read().strip().split('\n'),
-                 (1, []))
-print('part 1:', sum(i * hist[i] for i in range(20,221,40)))
+# Yeah I could do this with a for loop but where's the fun in that?
+_, history = reduce(lambda xh, i:
+                        (xh[0] + int(i[5:]), xh[1] + [xh[0], xh[0]])
+                        if i[0] == 'a'
+                        else (xh[0], xh[1] + [xh[0]]),
+                    open('day10_input.txt').read().strip().split('\n'),
+                    (1, []))
+print('part 1:', sum(i * history[i] for i in range(20,221,40)))
 print('part 2:')
-print('\n'.join(''.join('#' if abs(hist[x0+x]-x)<=1 
+print('\n'.join(''.join('#' if abs(history[x0 + x] - x) <= 1
                             else ' '
                         for x in range(40))
-                for x0 in range(0, len(hist), 40)))
+                for x0 in range(0, len(history), 40)))

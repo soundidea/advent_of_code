5935cdc5fa57115d72476bf444c7579eeb2700a8
Alistair Milne <soundidea@users.noreply.github.com>
2022-12-12 23:05:54 -0800

    tweak formatting

--- y2022/day13.py
+++ y2022/day13.py
@@ -1,36 +1,35 @@
 from functools import cmp_to_key
 from math import prod
 
-def compare(left, right, depth=0):
+def compare(left, right):
   if left == [] and right == []:
     return 0
-  elif left == []:
+  if left == []:
     return -1
-  elif right == []:
+  if right == []:
     return 1
-  elif type(left) != type(right):
-    if type(left) == int:
-      return compare([left], right, depth + 1)
-    else:
-      return compare(left, [right], depth + 1)
+  if type(left) != type(right):
+    return compare([left] if type(left) != list else left,
+                   [right] if type(right) != list else right)
   if type(left) == int:
     return -1 if left < right else (1 if right < left else 0)
   for l,r in zip(left, right):
-    cmp = compare(l, r, depth + 1)
+    cmp = compare(l, r)
     if cmp != 0:
       return cmp
   if len(left) < len(right):
     return -1
-  elif len(right) < len(left):
+  if len(right) < len(left):
     return 1
-  else:
-    return 0
+  return 0
 
-packets = [eval(p) for p in open('day13_input.txt').read().split('\n') if p != '']
+packets = [eval(p)
+           for p in open('day13_input.txt').read().split('\n')
+           if p != '']
 
-pair_compares = [compare(l, r) for (l,r) in zip(packets[:-1:2], packets[1::2])]
+pair_comps = [compare(l, r) for (l,r) in zip(packets[:-1:2], packets[1::2])]
 print('part 1:', sum(idx+1
-                     for idx,c in enumerate(pair_compares)
+                     for idx,c in enumerate(pair_comps)
                      if c < 0))
 
 dividers = [[[2]],[[6]]]

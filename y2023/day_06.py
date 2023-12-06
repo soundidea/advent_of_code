4ad663d540554f4673bf775a250c46e3524f2c61
Alistair Milne <soundidea@users.noreply.github.com>
2023-12-05 22:29:58 -0800

    Big optimization, recognizing the shape of the curve and just measuring the whole number distance between the roots

--- y2023/day_06.py
+++ y2023/day_06.py
@@ -1,3 +1,4 @@
+import math
 
 def parse_data(filename):
   lines = open(filename).readlines()
@@ -8,18 +9,29 @@ def parse_data(filename):
 data = parse_data('day_06_input.txt')
 test_data = parse_data('day_06_test_input.txt')
 
+def quadratic_roots(a, b, c):
+  det = math.sqrt(b**2 - (4 * a * c))
+  if det <= 0:
+    return []
+  elif det == 0:
+    return [-b / (2 * a)]
+  else:
+    return [(-b + det) / (2 * a), (-b - det) / (2 * a)]
+
 def part1(test=False):
   times, distances = test_data if test else data
   product = 1
-  for idx,t in enumerate(times):
-    product *= sum((t-i)*i > distances[idx] for i in range(1,t))
+  for idx in range(len((times))):
+    roots = quadratic_roots(-1, times[idx], -distances[idx] - 1)
+    product *= math.floor(roots[1]) - math.ceil(roots[0]) + 1
   return product
 
 def part2(test=False):
   times, distances = test_data if test else data
   time = int(''.join(map(str, times)))
   distance = int(''.join(list(map(str, distances))))
-  return sum((time-i)*i > distance for i in range(1,time))
+  roots = quadratic_roots(-1, time, -distance)
+  return math.floor(roots[1] - 0.0001) - math.ceil(roots[0] + 0.0001) + 1
 
 if __name__ == '__main__':
   p1_test = part1(test=True)

53d51afca7ea3ab6cdfca597385a4f1ad6daf073
Alistair Milne <soundidea@users.noreply.github.com>
2023-12-05 22:33:50 -0800

    calculate roots at d+1 not d

--- y2023/day_06.py
+++ y2023/day_06.py
@@ -22,7 +22,7 @@ def part1(test=False):
   times, distances = test_data if test else data
   product = 1
   for idx in range(len((times))):
-    roots = quadratic_roots(-1, times[idx], -distances[idx] - 1)
+    roots = quadratic_roots(-1, times[idx], -(distances[idx] + 1))
     product *= math.floor(roots[1]) - math.ceil(roots[0]) + 1
   return product
 
@@ -30,8 +30,8 @@ def part2(test=False):
   times, distances = test_data if test else data
   time = int(''.join(map(str, times)))
   distance = int(''.join(list(map(str, distances))))
-  roots = quadratic_roots(-1, time, -distance)
-  return math.floor(roots[1] - 0.0001) - math.ceil(roots[0] + 0.0001) + 1
+  roots = quadratic_roots(-1, time, -(distance + 1))
+  return math.floor(roots[1]) - math.ceil(roots[0]) + 1
 
 if __name__ == '__main__':
   p1_test = part1(test=True)

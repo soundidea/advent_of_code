ea49d6e8be95bfa65d90888424b90f37f4478c93
Alistair Milne <soundidea@users.noreply.github.com>
2023-12-07 10:47:52 -0800

    Some tidying-up, nothing substantially different

--- y2023/day_07.py
+++ y2023/day_07.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
 def parse_data(filename):
+  # "KK677 28" becomes: ([13,13,6,7,7], 28)
   letter_card_ranks = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
   data = [(list(hand), int(bid)) for hand, bid in map(str.split, open(filename).readlines())]
   return [([int(h) if h.isdigit() else letter_card_ranks[h] for h in hand], bid) for hand,bid in data]
@@ -8,55 +9,58 @@ def parse_data(filename):
 data = parse_data('day_07_input.txt')
 test_data = parse_data('day_07_test_input.txt')
 
-def type_rank(hand):
-  c = Counter(hand)
-  max_v, l, num_jokers = max(c.values()), len(c), c[1]
-  if l == 1:
-    return 7  # five of a kind
-  if max_v == 4:
+def rank(hand):
+  # Returns a tuple: (hand type, hand[0][0], hand[0][1], ...)
+  c = Counter(hand[0])
+  max_card_frequency = max(c.values())
+  num_unique_cards = len(c)
+  num_jokers = c[1]
+  if num_unique_cards == 1:
+    hand_type = 7  # five of a kind
+  elif max_card_frequency == 4:
     if num_jokers > 0:
-      return 7  # can be promoted to 5 of a kind (JJJJx or xxxxJ)
-    return 6  # four of a kind
-  if max_v == 3:
-    if min(c.values()) == 2:
+      hand_type = 7  # can be promoted to 5 of a kind (JJJJx or xxxxJ)
+    else:
+      hand_type = 6  # four of a kind
+  elif max_card_frequency == 3:
+    if num_unique_cards == 2:
       if num_jokers > 0:
-        return 7  # can be promoted to 5 of a kind (JJJxx or xxxJJ)
-      return 5  # full house
+        hand_type = 7  # can be promoted to 5 of a kind (JJJxx or xxxJJ)
+      else:
+        hand_type = 5  # full house
     else:
       if num_jokers > 0:
-        return 6  # can be promoted to 4 of a kind (JJJxy or xxxJy)
-      return 4  # three of a kind
-  if max_v == 2:
-    if l == 3:
+        hand_type = 6  # can be promoted to 4 of a kind (JJJxy or xxxJy)
+      else:
+        hand_type = 4  # three of a kind
+  elif max_card_frequency == 2:
+    if num_unique_cards == 3:
       if num_jokers == 2:
-        return 6  # can be promoted to 4 of a kind (JJxxy)
-      if num_jokers == 1:
-        return 5  # can be promoted to a full house (Jxxyy)
-      return 3  # two pair
+        hand_type = 6  # can be promoted to 4 of a kind (JJxxy)
+      elif num_jokers == 1:
+        hand_type = 5  # can be promoted to a full house (Jxxyy)
+      else:
+        hand_type = 3  # two pair
     else:
       if num_jokers > 0:
-        return 4  # can be promoted to 3 of a kind (JJxyz or Jxxyz)
-      return 2  # one pair
-  if num_jokers == 1:
-    return 2  # can be promoted to one pair (Jxyzw)
-  return 1  # high card
-
-def cmp(hand):
-  hand = hand[0]
-  key = type_rank(hand)
-  for card in hand:
-    key = 15 * key + card
-  return key
+        hand_type = 4  # can be promoted to 3 of a kind (JJxyz or Jxxyz)
+      else:
+        hand_type = 2  # one pair
+  elif num_jokers == 1:
+    hand_type = 2  # can be promoted to one pair (Jxyzw)
+  else:
+    hand_type = 1  # high card
+  return tuple([hand_type] + hand[0])
 
 def part1(test=False):
   hands = test_data if test else data
-  return sum(bid * (idx + 1) for idx, (_, bid) in enumerate(sorted(hands, key=cmp)))
+  return sum(bid * (idx + 1) for idx, (_, bid) in enumerate(sorted(hands, key=rank)))
 
 def part2(test=False):
   hands = test_data if test else data
   # Convert all the 11's (Jacks) to 1's (Jokers)
   hands = [([1 if c == 11 else c for c in hand], bid) for hand,bid in hands]
-  return sum(bid * (idx + 1) for idx, (_, bid) in enumerate(sorted(hands, key=cmp)))
+  return sum(bid * (idx + 1) for idx, (_, bid) in enumerate(sorted(hands, key=rank)))
 
 if __name__ == '__main__':
   p1_test = part1(test=True)

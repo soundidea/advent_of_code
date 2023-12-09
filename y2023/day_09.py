9a08cdd8fc0cb8fd550bb0839618d2ee1625a99a
Alistair Milne <soundidea@users.noreply.github.com>
2023-12-08 23:52:06 -0800

    Just cache the chains in parse_data

--- y2023/day_09.py
+++ y2023/day_09.py
@@ -1,16 +1,6 @@
 from functools import reduce
 from time import perf_counter
 
-def parse_data(filename):
-  return [list(map(int,l.split())) for l in open(filename).readlines()]
-
-st = perf_counter()
-data = parse_data('day_09_input.txt')
-parse_time = 1000 * (perf_counter() - st)
-test_data = parse_data('day_09_test_input.txt') 
-
-cached_chains = []
-
 def diffchain(sequence):
   chain, prev, l = [sequence], sequence, len(sequence)
   while True:
@@ -19,21 +9,29 @@ def diffchain(sequence):
     if not prev[-1]:
       break
     chain.append(prev)
-  cached_chains.append(chain)
   return chain
 
+def parse_data(filename):
+  sequences = [list(map(int,l.split())) for l in open(filename).readlines()]
+  return [diffchain(seq) for seq in sequences]
+
+st = perf_counter()
+data = parse_data('day_09_input.txt')
+parse_time = 1000 * (perf_counter() - st)
+test_data = parse_data('day_09_test_input.txt') 
+
 def part1(test=False):
-  sequences = test_data if test else data
-  return sum(sum(c[-1] for c in diffchain(s)) for s in sequences)
+  chains = test_data if test else data
+  return sum(sum(c[-1] for c in chain) for chain in chains)
 
 def part2(test=False):
-    return sum(reduce(lambda extend, seq: seq[0] - extend, chain[::-1], 0)
-               for chain in cached_chains)
+  chains = test_data if test else data
+  return sum(reduce(lambda extend, seq: seq[0] - extend, chain[::-1], 0)
+             for chain in chains)
 
 if __name__ == '__main__':
   p1_test = part1(test=True)
   p2_test = part2(test=True)
-  cached_chains = []
   st = perf_counter()
   p1 = part1()
   p1_time = 1000 * (perf_counter() - st)
